874d23287d1394dc9cf6e7fc5efd18b074d81168
irfansadiqdevzone <irfansadiq030@gmail.com>
2022-07-02 11:13:55 +0500

    removed use navigate

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './ForgotPassword.css'
-import { Link,useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const ForgotPassword = () => {
     return (
