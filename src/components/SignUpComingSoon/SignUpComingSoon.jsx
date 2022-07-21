cd9a0badd546de8e34cfae3f1f87f17137cc1203
Muhammad Yasir <yasir@systemplus.co>
2022-07-21 12:00:15 +0500

    api integration in coming soon

--- src/components/SignUpComingSoon/SignUpComingSoon.jsx
+++ src/components/SignUpComingSoon/SignUpComingSoon.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './SignUpComingSoon.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 
 const SignUpComingSoon = () => {
+    const emailField = useRef(null);
+    const navigate = useNavigate()
+
+    const handleSubmit = async () => {
+
+        const value = emailField.current?.value?.trim()
+
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+
+            const formData = new FormData();
+            formData.append('cmd', 'acl_User.setSignUpEmail');
+            formData.append('email', value);
+
+            try {
+                const { data } = await axios.post("https://portal.americau.com/woogi/0.1/actor/cgi.php", formData);
+                if (data == 200) {
+                    navigate("/thankyou");
+                } else
+                    navigate("/messageviewer", { msg: "We are sorry, there is some problem" })
+
+            } catch (error) {
+                window.alert(error.message)
+            }
+        } else {
+            window.alert("Please enter valid email address")
+        }
+    }
     return (
         <>
             <div className="forgot-password-container sign-up-comingsoon-container mx-auto flex flex-col">
@@ -17,21 +46,20 @@ const SignUpComingSoon = () => {
 
                 {/* Box Content Container */}
                 <main className='signup-comingsoon-main flex-col flex items-center justify-center'>
-                    <form action="/" className='w-full login-form flex justify-between flex-col'>
+                    <div className='w-full login-form flex justify-between flex-col'>
                         <div>
                             <h1 className="info-text">
                                 Enter your Email to be notified when sign ups are open to be public
                             </h1>
                         </div>
                         <div className="">
-                            <input name="email" type="text" placeholder="Email Address" className="w-full input-field mb-2" />
+                            <input ref={emailField} name="email" type="text" placeholder="Email Address" className="w-full input-field mb-2" />
                         </div>
 
                         <div className="">
-                            <Link to="/thankyou"><button className='btn-americau green-btn w-full uppercase'>Confirm</button></Link>
-
+                            <button onClick={handleSubmit} className='btn-americau green-btn w-full uppercase'>Confirm</button>
                         </div>
-                    </form>
+                    </div>
 
                 </main>
             </div>
